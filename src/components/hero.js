17e15c92a1c179d83a47529a2b9736f6a92ea20d
Kyle Gardner <kylegardner@Kyles-MacBook-Pro.local>
2019-09-14 11:55:24 -0400

    updated hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -23,20 +23,32 @@ const Title = styled.h1`
   font-family: buena-park-jf, serif;
   max-width: 1100px;
   line-height: 140%;
+  transition: .4s all ease-in-out;
 
 @media (max-width: 768px) {
   font-size: 2rem;
 }
 `
 
+const AboutLink = styled(props => <Link {...props} />)`
+  color: #ffffff;
+  text-decoration: none;
+  font-family: buena-park-jf, serif;
+  font-size: 1.5rem;
+
+  &::hover {
+    color: #FF0086;
+  }
+`
+
 const Hero = () => (
     <Container>
-      <Tween from={{ y: '100px', opacity: 0, }} to={{ y: '0px', color: 'white', opacity: 0.9 }} duration="0.9">
+      <Tween from={{ y: '100px', opacity: 0, delay: 0.2 }} to={{ y: '0px', color: 'white', opacity: 0.9 }} duration="0.7">
         <Title>neal gardner is a creative professional with experience building digital and physical products.</Title>
       </Tween>
       <Controller>
       <Scene duration={100} pin>
-      <Link style={{color:`white`, textDecoration: `none`, fontSize: `24px`, fontFamily: `buena-park-jf, serif` }} to="/about">About me</Link>
+          <AboutLink to="/about">About me</AboutLink>
       </Scene>
     </Controller>
     </Container>
