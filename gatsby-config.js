9638438eec48b48bf636cf90b7c3ca359336d8b2
Neal P Gardner <nealpgardner@gmail.com>
2019-09-18 14:50:09 -0400

    updated GA code

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+        trackingId: "UA-39300092-2",
         head: false,
       },
     },
