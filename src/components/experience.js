cd4369a21c80df0e0fd6e3f1109bd1e0bfe04f6d
Neal P Gardner <nealpgardner@gmail.com>
2019-09-18 11:48:56 -0400

    updated experience content

--- src/components/experience.js
+++ src/components/experience.js
@@ -60,19 +60,22 @@ const Experience = ({ siteTitle }) => (
        <ExperienceSection>
            <SingleExperience>
                 <h4>Samplify Bio 🧬</h4>
-                <h6>Operations Manager | August 2017 - May 2019</h6>
+                <h6>Operations Manager | Beverly, MA | 08/17 - 05/19</h6>
                 <p>&bull; Managed day-to-day operations, production, procurement and delivery of DNA testing products for multiple customers worldwide.</p>
-                <p>&bull; Managed day-to-day operations, production, procurement and delivery of DNA testing products for multiple customers worldwide.</p>
+                <p>&bull; Owned and maintained ISO 13485 quality management system for a CE IVD medical device. Established and
+maintained product and process documentation, work instructions, and risk validations.</p>
            </SingleExperience>
            <SingleExperience>
                 <h4>Veritas Genetics 🧬</h4>
-                <h6>Marketing & Brand | November 2015 - February 2017</h6>
-                <p>This is the description of what i did at that company</p>
+                <h6>Marketing & Brand Development | Danvers, MA | 11/15 - 02/17</h6>
+                <p>&bull; Led cross functional collaboration of engineering, business development, and sales teams to define product requirements for multiple genetic testing reports. Responsible for managing roadmap, and prioritizing backlog.</p>
+                <p>&bull; Designed, wireframed, and prototyped UX/UI for MVP of myGenome, a whole genome sequencing report. Worked with experience design (XD) team in-house at DigitasLBI for over six weeks of UX/UI design sprints.</p>
+                <p>&bull; Owned, produced, and maintained customer facing documents and initial brand guidelines. Established design system to ensure consistency was maintained throughout organization.</p>
            </SingleExperience>
            <SingleExperience>
                 <h4>Samplify Bio 🧬</h4>
                 <h6>Business Development | Some time to another time</h6>
-                <p>First employee to help commercialize a saliva collection device for genetic testing companies.</p>
+                <p>&bull; Managed day-to-day operations, production, procurement and delivery of DNA testing products for multiple customers worldwide.</p>
            </SingleExperience>
            <SingleExperience>
                 <h4>Supshot ☠️</h4>
